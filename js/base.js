33ef27de536e07e64dae72349ac9019aba73d254
JerryXia <gqkzwy@gmail.com>
2013-09-28 14:29:37 +0800

    add base.js

---
+++ js/base.js
@@ -0,0 +1,8 @@
+﻿var GLOBAL = {};
+GLOBAL.namespace = function(str){
+    var arr = str.split("."),o = GLOBAL;
+    for (i=(arr[0] == "GLOBAL") ? 1 : 0; i<arr.length; i++) {
+        o[arr[i]]=o[arr[i]] || {};
+        o=o[arr[i]];
+    }
+}
