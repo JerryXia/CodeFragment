098b342a927089f8d906719d6056bbc4e38ef053
JerryXia <gqkzwy@gmail.com>
2013-11-27 07:50:20 +0800

    udpate base function

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 /*!
 * GLOBAL namespace
 * Date: 2013-09-28
-*
+* 全局命名空间 生成公共变量
 *
 */
 var GLOBAL = {};
@@ -214,3 +214,38 @@ if (typeof Date.prototype.format == 'undefined') {
     };
 }
 
+/*!
+* Event
+* Date: 2013-11-26
+*
+*
+*/
+function addEvent(node, type, handler) {
+    if (!node) return false;
+    if (node.addEventListener) {
+        node.addEventListener(type, handler, false);
+        return true;
+    }
+    else if (node.attachEvent) {
+        node['e' + type + handler] = handler;
+        node[type + handler] = function() {
+            node['e' + type + handler](window.event);
+        };
+        node.attachEvent('on' + type, node[type + handler]);
+        return true;
+    }
+    return false;
+}
+
+function removeEvent(node, type, handler) {
+    if (!node) return false;
+    if (node.removeEventListener) {
+        node.removeEventListener(type, handler, false);
+        return true;
+    }
+    else if (node.detachEvent) {
+        node.detachEvent('on' + type, node[type + handler]);
+        node[type + handler] = null;
+    }
+    return false;
+}
