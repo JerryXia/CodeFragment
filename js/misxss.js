8c195b01efacceb6ea3fb3e92ee2575ddfb7e6fb
JerryXia <gqkzwy@gmail.com>
2014-04-23 01:07:04 +0800

    misxss test

--- js/misxss.js
+++ js/misxss.js
@@ -2,11 +2,13 @@ $(function(){
   var myleftDate = new Date(2014, 4, 21, 23, 59, 59);
   var myCusDate = new Date();
   if(myCusDate > myleftDate){
+    console.log(1);
     var $cnt = $('#cnt');
     if($cnt.length > 1){
       $cnt.children().remove();
     }
   }
+  console.log(2);
 });
 
 
