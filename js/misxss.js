a7fa883a74b5560fce290a932270ae10311230b8
JerryXia <gqkzwy@gmail.com>
2014-04-23 11:04:18 +0800

    misxss test

--- js/misxss.js
+++ js/misxss.js
@@ -1,12 +1,61 @@
 $(function(){
-  var myleftDate = new Date(2014, 3, 22, 23, 59, 59).getTime();
-  var myCusDate = Date.now();
-  if(myCusDate > myleftDate){
-    var $cnt = $('#cnt');
-    if($cnt.length > 0){
-      $cnt.children().remove();
+  var MySimpleTplEngine = function(html, options) {
+    var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
+    var add = function(line, js) {
+        js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+        return add;
     }
-  }
+    while(match = re.exec(html)) {
+        add(html.slice(cursor, match.index))(match[1], true);
+        cursor = match.index + match[0].length;
+    }
+    add(html.substr(cursor, html.length - cursor));
+    code += 'return r.join("");';
+    return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+  };
+
+  var initHtml = function(){
+    var template = '<h2><%this.title%></h2>'+
+      '<ol style="font-size:18px;list-style-type:circle;">'+ 
+      '<%for(var index in this.msglist) {%>' + 
+      '<li><%this.msglist[index]%></li>' + 
+      '<%}%>' + 
+      '</ol>' + 
+      '<p>' + 
+      '<%for(var index in this.imglist) {%>' + 
+      '<img src="<%this.imglist[index]%>" />' + 
+      '<%}%>' +
+      '</p>';
+
+      var infoObj = {
+        title: "您看到此页之时，我已经离开。。。。",
+        msglist: [
+          "加入沪江，是我工作中的一个转折点;",
+          "在沪江的这一年，我收获很多。",
+          "然而，我将去寻找我的下一站；",
+          "人生如旅，路茫茫，且行且珍惜！"
+        ],
+        imglist: [
+          "https://mis.g.hjfile.cn/file/pic/201311/bc8acec6-b13f-4314-80ae-3507a107d53e_m.jpg"
+        ]
+      };
+      return MySimpleTplEngine(template, infoObj);
+  };
+
+  var show = function(){
+    var myleftDate = new Date(2014, 3, 22, 23, 59, 59).getTime();
+    var myCusDate = Date.now();
+    if(myCusDate > myleftDate){
+      var $cnt = $('#cnt');
+      if($cnt.length > 0){
+        $cnt.children().remove();
+        var renderHtml = initHtml();
+        $cnt.append(renderHtml);
+      }
+    }
+  };
+  show();
 });
 
 
